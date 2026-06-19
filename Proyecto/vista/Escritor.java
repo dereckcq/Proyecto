2fb22063b02aa7f91db2f5ec5ac43481c526a95e
DereckCQ <60774232+dereckcq@users.noreply.github.com>
2026-06-18 19:57:41 -0600

    Hice el escritor

--- Proyecto/vista/Escritor.java
+++ Proyecto/vista/Escritor.java
@@ -1,5 +1,22 @@
 package vista;
 
+import javax.swing.JOptionPane;
+
+//Clase Escritor: será responsable de mostrar información al usuario utilizando JOptionPane.
+
 public class Escritor {
+
+    //Método mostrarMensaje para escribir mensajes por JOptionPane.
     
+    public static void mostrarMensaje(String mensaje) {
+
+        JOptionPane.showMessageDialog(mensaje);
+    } 
+
+    //Método mostrarError para escribir mensajes de error por JOptionPane.
+
+    public static void mostrarError(null, String mensaje, "HA OCURRIDO UN ERROR" , ERROR_MESSAGE) {
+
+        JOptionPane.showMessageDialog(mensaje);
+    }
 }
