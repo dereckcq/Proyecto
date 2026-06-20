adbaf7c131581e5754ef598d8cec216c46b38a5d
Mymilita <caminao.ram@gmail.com>
2026-06-20 16:31:36 -0600

    correciones

--- Proyecto/modelo/Arma.java
+++ Proyecto/modelo/Arma.java
@@ -1,91 +1,82 @@
 package modelo;
 
 public class Arma {
-
-    private String nombre;
-    private int danioMinimo;
-    private Int danioMaximo;
-
+  
+  private String nombre;
+  private int danioMinimo;
+  private int danioMaximo;
+  
+  
+  // Constructor con parametros 
+  public Arma (String nombre , int danioMaximo , int danioMinimo){
     
-    // Constructor con parametros 
-    public Arma (String nombre , int danioMaximo , int danioMinimo){
-
-        if (nombre == null || nombre.equals("")) {
-            this.nombre = " noName ";
-        } else {
-            this.nombre = nombre;
-        }
-
-        if (danioMaximo > 100 || danioMaximo < 0 )
-
-            this.danioMaximo = 100;
-        } else {
-            this.danioMaximo = danioMaximo;
-        } 
-
-        if (danioMinimo > 100 || danioMinimo < 0 ){
-
-            this.danioMinimo = 1;
-        } else {
-            this.danioMinimo = danioMinimo;
-        } 
-
-        //metodo Para generar danio aleatorio 
-    public int generarDanio() {
-        Random random = new Random();
-        return random.nextInt(danioMaximo - danioMinimo + 1) + danioMinimo;
-    }
-
-
-    //Metodos set 
-    public void setNombre(String nombre) {
-        if (nombre == null || Nombre.equals("")) {
-                this.Nombre = "noName";
-        } else {
-            this.nombre = nombre;
-        }
-    }   
-
-    public void setDanioMaximo (int danioMaximo){
-        if (danioMaximo > 100 || danioMaximo < 0 ){
-
-            this.danioMaximo = 100;
-        } else {
-            this.danioMaximo = danioMaximo;
-        } 
-
-    }
-
-    public void setDanioMinimo (int danioMinimo){
-        if (danioMinimo > 100 || danioMinimo < 0 ){
-
-            this.danioMinimo = 100;
-        } else {
-            this.danioMinimo = danioMinimo;
-        } 
-    }
-
-
-
-    // Metodos get 
-    public String Nombre() {
-        return nombre
+    if (nombre != null && !nombre.equals("")) {
+      this.nombre = nombre;
+    }else{
+      this.nombre = "sin nombre";
     }
     
-    public int danioMaximo() {
-        return danioMaximo
-    }
+    if (danioMaximo >= 0 && danioMaximo <= 100 ) {
+      this.danioMaximo = danioMaximo;
+    }else{
+      this.danioMaximo = 100;
+    } 
     
-    public int danioMinimo) {
-        return danioMinimo
+    if (danioMinimo >= 0 && danioMinimo <= 100 ) {
+      this.danioMinimo = danioMinimo;
+    }else{
+      this.danioMinimo = 1;
+    } 
+  }
+  
+  //metodo Para generar danio aleatorio 
+  public int generarDanio() {
+    int danioGenerado = (int)(Math.random() * (danioMaximo - danioMinimo + 1)) + danioMinimo;
+    return danioGenerado;
+  }
+  
+  
+  //Metodos set 
+  public void setNombre(String nombre) {
+    if (nombre != null && !nombre.equals("")) {
+      this.nombre = nombre;
+    }else{
+      this.nombre = "sin nombre";
     }
-
-    //Metodo toString
-
-    @Override
-    public String toString() {
-    Return " Nombre: " + nombre+ ", DanioMaximo: " + danioMaximo + ", Danio minimo: " + danioMinimo;
-    }
-
-    
+  }   
+  
+  public void setDanioMaximo (int danioMaximo){
+    if (danioMaximo >= 0 && danioMaximo <= 100 ){
+      this.danioMaximo = danioMaximo;
+    }else{
+      this.danioMaximo = 100;
+    } 
+  }
+  
+  public void setDanioMinimo (int danioMinimo){
+    if (danioMinimo >= 0 && danioMinimo <= 100 ){
+      this.danioMinimo = danioMinimo
+    }else{
+        this.danioMinimo = 1;
+      }
+  }
+  
+  // Metodos get 
+  public String getNombre() {
+    return nombre;
+  }
+  
+  public int getDanioMaximo() {
+    return danioMaximo;
+  }
+  
+  public int getDanioMinimo() {
+    return danioMinimo;
+  }
+  
+  //Metodo toString
+  @Override
+  public String toString() {
+    return "Nombre: " + nombre + "\nDanioMaximo: " + danioMaximo + "\nDanio minimo: " + danioMinimo;
+  }
 }
