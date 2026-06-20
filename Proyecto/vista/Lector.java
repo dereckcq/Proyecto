8a7711ddcc91110deb3deffff7ed8d14056a7d1a
Mymilita <caminao.ram@gmail.com>
2026-06-20 15:03:25 -0600

    Lector ya hecho

--- Proyecto/vista/Lector.java
+++ Proyecto/vista/Lector.java
@@ -1,5 +1,4 @@
 package vista;
-
 import javax.swing.JOptionPane;
 
 //Clase Lector: será responsable de solicitar datos al usuario utilizando JOptionPane. 
@@ -8,35 +7,53 @@ import javax.swing.JOptionPane;
 public class Lector {
 
     //Método leerTexto para leer mensajes tipo String por JOptionPane.
-
     public static String leerTexto(String mensaje) {
-
-        String texto = JOptionPane.showInputDialog(mensaje);
-
-        if (texto == null && != texto.equals("") && ) {
-
-        } else {
-
+        String texto;
+  
+        do {
+            texto = JOptionPane.showInputDialog(mensaje);
+    
+            if (texto == null || texto.equals("")) {
+                JOptionPane.showMessageDialog(null, "No puede dejar el espacio vacio");
+            }
+    
+        } while (texto == null || texto.equals(""));
+  
         return texto;
-    }
+    } 
 
-     //Método leerInt para leer mensajes tipo int por JOptionPane.
-
-
+    //Método leerInt para leer mensajes tipo int por JOptionPane.
     public static int leerInt(String mensaje) {
-
-        int numero = JOptionPane.showInputDialog(mensaje);
-
+        int numero = 0;
+        boolean valido = false;
+  
+        while (!valido) {
+    
+            try {
+                String texto = JOptionPane.showInputDialog(mensaje);
+                numero = Integer.parseInt(texto);
+                valido = true;
+      
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null, "Debe ingresar un número.");
+            }
+        }
         return numero;
-
     }
 
     //Método leerOpcion para el ingreso de opciones por JOptionPane.
-
     public static int leerOpcion(String mensaje, int minimo, int maximo) {
-
-        int opcion 
-
+       int opcion;
+  
+        do {
+            opcion = leerInt(mensaje);
+    
+            if (opcion < minimo || opcion > maximo) {
+                JOptionPane.showMessageDialog(null, "Opcion invalida debe ingresar una opcion entre " + minimo + " y " + maximo);
+            }
+    
+        } while (opcion < minimo || opcion > maximo);
+  
+        return opcion;
     }
-}
 }
