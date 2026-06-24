e2d1983445cd2d06795c16d21a6b29675ed6f156
DereckCQ <60774232+dereckcq@users.noreply.github.com>
2026-06-23 21:17:02 -0600

    Excepciones

---
+++ excepciones/ExcepcionValorNegativo.java
@@ -0,0 +1,8 @@
+package excepciones;
+
+public class ExcepcionValorNegativo extends Exception {
+  
+  public ExcepcionValorNegativo(String msg) {
+    super(msg);
+  }
+}
---
+++ excepciones/ExcepcionValorNulo.java
@@ -0,0 +1,8 @@
+package excepciones;
+
+public class ExcepcionValorNulo extends Exception {
+  
+  public ExcepcionValorNulo(String msg) {
+    super(msg);
+  }
+}
